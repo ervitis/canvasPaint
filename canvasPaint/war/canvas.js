5b4706579a0972d2d0022ae4ba52fd64b08eb763
v <vitomarti@gmail.com>
2014-03-03 18:00:25 +0100

    Added new buttons: clean and close the drew sharp.

--- canvasPaint/war/canvas.js
+++ canvasPaint/war/canvas.js
@@ -1,5 +1,5 @@
 /**
- * @author VMA
+ * @author victor
  * 
  * Tool for Canvas drawing
  */
@@ -8,6 +8,9 @@ var canvas = null;
 var contextCanvas = null;
 var puntos;
 
+/**
+ * Initialize the tool
+ */
 function initialize(){	
 	if (canvas === null){
 		canvas = document.getElementById("areaDibujo");
@@ -20,19 +23,59 @@ function initialize(){
 	}
 }
 
+/**
+ * Draw the sharp by clicking on the canvas area
+ * @param event
+ */
 function drawSharp(event){
 	var dot;
 	
-	dot = event.offsetX + ',' + event.offsetY;
-	puntos.push(dot);
-	console.log(puntos.length, dot);
+	if (!canvas){
+		initialize();
+	}
 	
-	
-	if (puntos.length > 1){
+	if (puntos.length > 0){
 		contextCanvas.lineTo(event.offsetX, event.offsetY);
 		contextCanvas.stroke();
 	}
 	else{
 		contextCanvas.moveTo(event.offsetX, event.offsetY);
 	}
+	dot = event.offsetX + ',' + event.offsetY;
+	puntos.push(dot);
+}
+
+/**
+ * Close the sharp by adding the last line on the polygon
+ */
+function closeShape(){
+	var finalX, finalY;
+	var inicioX, inicioY;
+	
+	if (puntos.length > 2){
+		finalX = puntos[puntos.length-1].split(',')[0];
+		finalY = puntos[puntos.length-1].split(',')[1];
+		console.log(finalX, finalY);
+		inicioX = puntos[0].split(',')[0];
+		inicioY = puntos[0].split(',')[1];
+		
+		contextCanvas.moveTo(finalX, finalY);
+		contextCanvas.lineTo(inicioX, inicioY);
+		contextCanvas.stroke();
+	}
+	else{
+		alert("You drew only a line");
+	}
+	
+}
+
+/**
+ * Clean the shape
+ * @see http://stackoverflow.com/questions/2142535/how-to-clear-the-canvas-for-redrawing for the canvas cleaning
+ */
+function cleanShape(){
+	canvas.width = canvas.width;
+	contextCanvas = null;
+	canvas = null;
+	puntos = null;
 }
