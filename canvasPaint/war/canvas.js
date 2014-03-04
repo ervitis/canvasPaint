9ec37a203486bbcb13839dc146a9543ce91a4793
v <vitomarti@gmail.com>
2014-03-05 00:10:11 +0100

    You can write a decimal number to set the measure. It has a bug which will be fixed in the next nightly release.

--- canvasPaint/war/canvas.js
+++ canvasPaint/war/canvas.js
@@ -47,23 +47,26 @@ function drawSharp(event){
 										puntos[pointerCounter-1].split(',')[1],
 										event.offsetX,
 										event.offsetY));
+				console.log(code);
 				switch(code){
 					
 					case 1:
-						contextCanvas.lineTo(event.offsetX, puntos[pointerCounter-1].split(',')[1]);
-						contextCanvas.stroke();
+						drawLine(contextCanvas, event.offsetX, puntos[pointerCounter-1].split(',')[1]);
+						showInput(event.offsetX, puntos[pointerCounter-1].split(',')[1]);
 						break;
 					case 2:
+						drawLine(contextCanvas, puntos[pointerCounter-1].split(',')[0], event.offsetY);
+						showInput(puntos[pointerCounter-1].split(',')[0], event.offsetY);
 						break;
-					case 3:
-						contextCanvas.lineTo(puntos[pointerCounter-1].split(',')[0], event.offsetY);
-						contextCanvas.stroke();
+					default:
+						drawLine(contextCanvas, event.offsetX, event.offsetY);
+						showInput(event.offsetX, event.offsetY);
 						break;
 				}
 			}
 			else{
-				contextCanvas.lineTo(event.offsetX, event.offsetY);
-				contextCanvas.stroke();
+				drawLine(contextCanvas, event.offsetX, event.offsetY);
+				showInput(event.offsetX, event.offsetY);
 			}
 		}
 		else{
@@ -71,8 +74,123 @@ function drawSharp(event){
 		}
 		dot = event.offsetX + ',' + event.offsetY;
 		puntos.push(dot);
-		//console.log(pointerCounter, puntos.length);
 		++pointerCounter;
+	}
+}
+
+/**
+ * Draw a line
+ * @param cc context
+ * @param x point
+ * @param y point
+ */
+function drawLine(cc, x, y){
+	cc.lineTo(x, y);
+	cc.stroke();
+}
+
+/**
+ * Show the input text
+ * @param x
+ * @param y
+ */
+function showInput(x, y){
+	var inputMedida = document.getElementById('inputMedida');
+	inputMedida.style.position = 'absolute';
+	inputMedida.style.left = x + 'px';
+	inputMedida.style.top = y + 'px';
+	inputMedida.style.display = 'inline';
+	document.getElementById('inputM').focus();
+}
+
+/**
+ * Hide input text
+ */
+function hideInput(){
+	var inputMedida = document.getElementById('inputMedida');
+	inputMedida.value = '';
+	inputMedida.style.display = 'none';
+}
+
+/**
+ * Get the measure from the input element
+ * @returns the value in float format
+ */
+function getMeasure(){
+	var measure = document.getElementById('inputM').value;
+	
+	if (measure.indexOf(',') > -1){
+		measure = measure.replace(',', '.');
+		measure = parseFloat(measure);
+	}
+	
+	if (!isNaN(measure)){
+		return parseFloat(measure);
+	}
+	return -1;
+}
+
+/**
+ * Write the measure inside the canvas
+ * @param cc the context
+ * @param x0
+ * @param y0
+ * @param x1
+ * @param y1
+ */
+function writeMeasure(cc, x0, y0, x1, y1){
+	var measure = getMeasure();
+	
+	console.log(x0, y0, x1, y1);
+	
+	if (measure !== -1){		
+		var distance = getDistance(x0, y0, x1, y1);
+		var alfa = Math.asin((y1 - y0) / distance);
+		console.log(distance, alfa);
+		var posY = parseInt((distance / 2) * Math.sin(alfa));
+		var posX = parseInt((distance / 2) * Math.cos(alfa));
+
+		console.log('antes ', posX, posY);
+		posY += parseInt(y0);
+		posX += parseInt(x0);
+		console.log('despues ', posX, posY);
+		
+		cc.font = '18px Arial';
+		
+		if (!isNaN(measure)){
+			measure = measure.toString();
+		}
+		
+		if (measure.indexOf('.') > -1){
+			measure = measure.replace('.', ',');
+		}
+		cc.fillText(measure, posX, posY);
+	}
+	else{
+		alert('Invalid measure');
+	}
+}
+
+/**
+ * Detect the 'intro' key and write the text in the canvas
+ * @param event
+ */
+function getMeasureIntro(event){
+	var code = event.which ? event.which : event.keyCode;
+	
+	if (code === 13){
+		if (canvas){
+			writeMeasure(
+					contextCanvas, 
+					puntos[pointerCounter-2].split(',')[0],
+					puntos[pointerCounter-2].split(',')[1],
+					puntos[pointerCounter-1].split(',')[0],
+					puntos[pointerCounter-1].split(',')[1]);
+			
+			var inputMedida = document.getElementById('inputMedida');
+			inputMedida.style.display = 'none';
+			inputMedida.children[0].value = '';
+		}
 	}
 }
 
@@ -95,6 +213,7 @@ function closeShape(){
 			contextCanvas.stroke();
 			
 			document.getElementById('idCloseShape').disabled = true;
+			hideInput();
 			clickable = false;
 		}
 		else{
@@ -114,6 +233,7 @@ function cleanShape(){
 	canvas = null;
 	puntos = null;
 	clickable = true;
+	document.getElementById('inputMedida').style.display = 'none';
 	document.getElementById('idCloseShape').disabled = false;
 }
 
@@ -135,10 +255,10 @@ function isShiftPressed(event){
  * @param y0 initial y-position
  * @param x1 last x-position
  * @param y1 last y-position
- * @returns 1 for horizontal, 2 for diagonal, 3 for vertical
+ * @returns 1 for horizontal, 2 for vertical, 0 for normal
  */
 function whichStraightLine(x0, y0, x1, y1){
-	var distance = Math.sqrt(Math.pow((x1 - x0), 2) + Math.pow((y1 - y0), 2));
+	var distance = getDistance(x0, y0, x1, y1);
 	var proyX, proyY;
 	var resArcCoseno;
 	
@@ -152,10 +272,27 @@ function whichStraightLine(x0, y0, x1, y1){
 			return 1;
 		}
 		else if (CONST_STRAIGHT_LINE * 2 < resArcCoseno){
-			return 3;
-		}
-		else{
 			return 2;
 		}
+		else{
+			return 0;
+		}
+	}
+}
+
+/**
+ * Calculate the distance between two points: (x0, y0) - (x1, y1)
+ * @param x0
+ * @param y0
+ * @param x1
+ * @param y1
+ * @returns the distance or -1 if error
+ */
+function getDistance(x0, y0, x1, y1){
+	try{
+		return Math.sqrt(Math.pow((x1 - x0), 2) + Math.pow((y1 - y0), 2));
+	}catch(e){
+		console.log(e);
+		return -1;
 	}
 }
